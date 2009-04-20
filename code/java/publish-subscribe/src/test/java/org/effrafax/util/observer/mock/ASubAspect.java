1a4a6ff5a01f3a72e26eea5e76bdd78b2f204672
daan.wanrooy <daan.wanrooy@a97aef7e-2cd9-11de-94d6-134fa40061eb>
2009-04-20 07:49:57 +0000

    Removed private classes from AbstractPublicationTest and moved them to a mock package.

---
+++ code/java/publish-subscribe/src/test/java/org/effrafax/util/observer/mock/ASubAspect.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.effrafax.util.observer.mock;
+
+import org.effrafax.util.observer.interfaces.PublicationAspect;
+
+/**
+ * @author dwanrooy
+ *
+ */
+public class ASubAspect extends AnAspect implements PublicationAspect {
+
+}
