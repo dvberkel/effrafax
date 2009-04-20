e733f664363262c57b3e08fb8fa937204c80e88d
daan.wanrooy <daan.wanrooy@a97aef7e-2cd9-11de-94d6-134fa40061eb>
2009-04-20 11:17:24 +0000

    added dependency on test framework

--- trunk/code/java/publish-subscribe/src/test/java/org/effrafax/util/observer/AbstractSubscriberTest.java
+++ trunk/code/java/publish-subscribe/src/test/java/org/effrafax/util/observer/AbstractSubscriberTest.java
@@ -3,8 +3,12 @@
  */
 package org.effrafax.util.observer;
 
+import org.effrafax.test.ExceptionInvoker;
+import org.effrafax.test.ExceptionTester;
+import org.effrafax.util.observer.implementations.AbstractSubscriber;
+import org.effrafax.util.observer.interfaces.Publisher;
+import org.effrafax.util.observer.interfaces.Subscriber;
 import org.junit.Test;
-
 
 /**
  * @author dwanrooy
@@ -13,8 +17,85 @@ import org.junit.Test;
 public class AbstractSubscriberTest {
 	
 	@Test
-	public void testHandlerRegistration() {
+	public void testHandlerRegistrationFailure() {
 		
+		ExceptionTester.testForException(
+			IllegalArgumentException.class, 
+			new ExceptionInvoker() {
+
+				@Override
+				public void invoke() throws Exception {
+					
+					/* Notice the anonymous class instantiation */
+					Subscriber subscriber = new AbstractSubscriber() {};
+					
+					subscriber.registerHandler(null, null);
+				}				
+			}
+		);
+	}
+	
+	@Test
+	public void testUpdateFailureFirstArgument() {
 		
+		ExceptionTester.testForException(
+			IllegalArgumentException.class, 
+			new ExceptionInvoker() {
+
+				@Override
+				public void invoke() throws Exception {
+					
+					/* Notice the anonymous class instantiation */
+					Subscriber subscriber = new AbstractSubscriber() {};
+					
+					subscriber.update(
+						null, 
+						null
+					);
+				}				
+			}
+		);
+	}
+	
+	@Test
+	public void testUpdateFailureSecondArgument() {
+		
+		ExceptionTester.testForException(
+			IllegalArgumentException.class, 
+			new ExceptionInvoker() {
+
+				@Override
+				public void invoke() throws Exception {
+					
+					/* Notice the anonymous class instantiation */
+					Subscriber subscriber = new AbstractSubscriber() {};
+					
+					subscriber.update(
+						new Publisher() {
+
+							@Override
+							public void attach(Subscriber subscriber) {
+								
+								/* This stub is for testing only */
+							}
+
+							@Override
+							public void detach(Subscriber subscriber) {
+								
+								/* This stub is for testing only */
+							}
+
+							@Override
+							public void publish() {
+								
+								/* This stub is for testing only */
+							}
+							
+						}, 
+						null
+					);
+				}				
+			}
+		);
 	}
 }
