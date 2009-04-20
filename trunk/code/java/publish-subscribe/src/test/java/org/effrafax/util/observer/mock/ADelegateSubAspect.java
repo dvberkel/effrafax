f74c4cafbd0ce73768db5512b962dac7c9a9c313
daan.wanrooy <daan.wanrooy@a97aef7e-2cd9-11de-94d6-134fa40061eb>
2009-04-20 08:06:19 +0000

    Created a test for DelegatePublicationAspect.

---
+++ trunk/code/java/publish-subscribe/src/test/java/org/effrafax/util/observer/mock/ADelegateSubAspect.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.effrafax.util.observer.mock;
+
+import org.effrafax.util.observer.interfaces.PublicationAspect;
+
+/**
+ * @author dwanrooy
+ *
+ */
+public class ADelegateSubAspect extends AnDelegateAspect implements
+		PublicationAspect {
+
+}
