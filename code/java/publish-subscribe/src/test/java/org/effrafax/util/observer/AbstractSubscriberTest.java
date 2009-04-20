a73f2c8e4997a316da46bf7f9ff9466ccbc6275f
daan.wanrooy <daan.wanrooy@a97aef7e-2cd9-11de-94d6-134fa40061eb>
2009-04-20 10:29:17 +0000

    Created a test for AbstractSubscriber.

---
+++ code/java/publish-subscribe/src/test/java/org/effrafax/util/observer/AbstractSubscriberTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.effrafax.util.observer;
+
+import org.junit.Test;
+
+
+/**
+ * @author dwanrooy
+ *
+ */
+public class AbstractSubscriberTest {
+	
+	@Test
+	public void testHandlerRegistration() {
+		
+		
+	}
+}
