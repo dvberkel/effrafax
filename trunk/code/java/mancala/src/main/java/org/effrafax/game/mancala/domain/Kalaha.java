090cac93671ab9dbe9064dfee3b31a32d22a8031
daan.wanrooy <daan.wanrooy@a97aef7e-2cd9-11de-94d6-134fa40061eb>
2009-05-27 08:30:10 +0000

    Created interfaces for the Bowl and Kalaha.

---
+++ trunk/code/java/mancala/src/main/java/org/effrafax/game/mancala/domain/Kalaha.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.effrafax.game.mancala.domain;
+
+/**
+ * This class represents a kalaha in the mancala game.
+ * 
+ * @author dwanrooy
+ *
+ */
+public interface Kalaha extends Bowl {
+
+}
