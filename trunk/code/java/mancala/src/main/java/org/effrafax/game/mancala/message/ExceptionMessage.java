b45fcdee26facb24cff58f05e23f2a22d7503736
daan.wanrooy <daan.wanrooy@a97aef7e-2cd9-11de-94d6-134fa40061eb>
2009-05-28 13:43:37 +0000

    Linked constructors of the ExceptionMessage enum.

--- trunk/code/java/mancala/src/main/java/org/effrafax/game/mancala/message/ExceptionMessage.java
+++ trunk/code/java/mancala/src/main/java/org/effrafax/game/mancala/message/ExceptionMessage.java
@@ -4,6 +4,8 @@
 package org.effrafax.game.mancala.message;
 
 /**
+ * This enum provides various error messages for the domain.
+ * 
  * @author dwanrooy
  *
  */
@@ -13,28 +15,37 @@ public enum ExceptionMessage {
 	NON_NEGATIVE("argument should be non-negative."),
 	NON_POSITIVE("argument should be greater then zero."),
 	NON_NULL_NEGATIVE(NON_NULL, NON_NEGATIVE),
+	ALREADY_ASSIGNED("argument is already assigned."),
 	TO_FEW_STONES("heap does not contain enough stones."),
 	NO_SUCH_OBJECT_FOR_KALAHA("there is no such a thing for a kalaha."),
-	BOWL_NULL("bowl should be non-null."),
-	BOWL_ASSIGNED("bowl is already assigned."),
-	OWNER_NULL("owner should be non-null."),
-	OWNER_ASSIGNED("owner is already assigned."),
+	BOWL("a bowl argument exception."),
+	BOWL_NULL(BOWL, NON_NULL),
+	BOWL_ASSIGNED(BOWL, ALREADY_ASSIGNED),
+	OWNER("a owner argument exception."),
+	OWNER_NULL(OWNER, NON_NULL),
+	OWNER_ASSIGNED(OWNER, ALREADY_ASSIGNED),
 	NOT_PLAYABLE("the bowl is not playable.");
 
 	private String message = "an exception has occured";
 	private ExceptionMessage[] exceptionMessages = {};	
 	private boolean messageGenerated = false;
 	
-	ExceptionMessage(String message) {
+	private ExceptionMessage(String message) {
 		
-		this.message = message;
+		this(message,new ExceptionMessage[] {});
 	}
 	
 	private ExceptionMessage(ExceptionMessage... exceptionMessages) {
 		
-		this.exceptionMessages = exceptionMessages;
+		this("", exceptionMessages);
 	}
+	
+	private ExceptionMessage(String message, ExceptionMessage... exceptionMessages) {
 
+		this.message = message;
+		this.exceptionMessages = exceptionMessages;		
+	}
+	
 	/* (non-Javadoc)
 	 * @see java.lang.Enum#toString()
 	 */
@@ -43,8 +54,7 @@ public enum ExceptionMessage {
 		
 		if (exceptionMessages.length != 0 && !messageGenerated) {
 			
-			message = exceptionMessages[0].toString();
-			for (int index = 1; index < exceptionMessages.length; index++) {
+			for (int index = 0; index < exceptionMessages.length; index++) {
 				
 				message += " " + exceptionMessages[index].toString();
 			}
