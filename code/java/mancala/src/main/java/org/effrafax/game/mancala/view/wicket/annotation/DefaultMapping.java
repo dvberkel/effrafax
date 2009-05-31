758828c9b5569fd5d4f751b11f3e15f059b8a709
daan.wanrooy <daan.wanrooy@a97aef7e-2cd9-11de-94d6-134fa40061eb>
2009-05-31 15:00:10 +0000

    This seems as a very large commit. I will explain why this seems so.
    First of all I prepared the project for the use of wicket. This
    entails various dependencies in the project pom, addition of 
    various directories and files.
    Furthermore I do not like the default behavior of wicket to
    search the corresponding html file in the same package
    structure as the java file. So I created an
    AnnotatedLocator.java which makes use of annotations to
    direct the wicket container to the correct container.
    Lastly I greeting a greeting file with a backing
    implementation to test the working of setup.

---
+++ code/java/mancala/src/main/java/org/effrafax/game/mancala/view/wicket/annotation/DefaultMapping.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.effrafax.game.mancala.view.wicket.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author dwanrooy
+ *
+ */
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface DefaultMapping {
+	
+	public String value();
+}
