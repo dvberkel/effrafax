384e91721da7bcf0b6ed7183eb573ac0ea4fbd9e
daan.wanrooy <daan.wanrooy@a97aef7e-2cd9-11de-94d6-134fa40061eb>
2009-06-02 11:14:01 +0000

    Used a live mancala game to fill the various parts of the subject page.

--- code/java/mancala/src/main/java/org/effrafax/game/mancala/view/wicket/Status.java
+++ code/java/mancala/src/main/java/org/effrafax/game/mancala/view/wicket/Status.java
@@ -3,8 +3,13 @@
  */
 package org.effrafax.game.mancala.view.wicket;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.effrafax.game.mancala.Mancala;
+import org.effrafax.game.mancala.MancalaBuilder;
 import org.effrafax.game.mancala.domain.Player;
 
 /**
@@ -14,22 +19,38 @@ import org.effrafax.game.mancala.domain.Player;
 public class Status extends WebPage {
 	
 	private static final String BOWL_ID = "BowlNr";
+	private static final String KALAHA_ID = "Kalaha";
+	
+	public Mancala getMancala() {
+		
+		MancalaBuilder builder = new MancalaBuilder();
+		builder.setStartPlayer(Player.white);
+		builder.setNumberOfBowls(6);
+		builder.setNumberOfStones(4);
+		
+		return new Mancala(builder);
+	}
 	
 	public Status() {
 		
-		add(new Label("plyCount", "0"));
-		add(new Label("currentPlayer", Player.white.toString()));
+		Mancala mancala = getMancala();
 		
+		add(new Label("plyCount", "0"));
+		add(new Label("currentPlayer", mancala.getCurrentPlayer().toString()));
+		
+		Map<Player,List<Integer>> stonesPerPlayer = mancala.getStonesPerPlayer();
 		for (Player player : Player.values()) {
 			
+			List<Integer> stones = stonesPerPlayer.get(player);
 			for (int index = 0; index < 6; index++) {
 				
 				String currentBowlId = player.toString() + BOWL_ID + index;
 				
-				add(new Label(currentBowlId, "4"));
+				add(new Label(currentBowlId, stones.get(index).toString()));
 			}
+			
+			String kalahaId = player.toString() + KALAHA_ID;
+			add(new Label(kalahaId, stones.get(6).toString()));
 		}
-		add(new Label("whiteKalaha", "0"));
-		add(new Label("blackKalaha", "0"));
 	}
 }
