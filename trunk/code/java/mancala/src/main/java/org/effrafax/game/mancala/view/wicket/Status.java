ebddcd6a01e43ebba41ec886be1ba39b809bc5cd
daan.wanrooy <daan.wanrooy@a97aef7e-2cd9-11de-94d6-134fa40061eb>
2009-06-02 18:35:40 +0000

    Created a all pages which control the mancala application. A rudimentary filling of code is present, albeit it is not complete.

--- trunk/code/java/mancala/src/main/java/org/effrafax/game/mancala/view/wicket/Status.java
+++ trunk/code/java/mancala/src/main/java/org/effrafax/game/mancala/view/wicket/Status.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.effrafax.game.mancala.Mancala;
 import org.effrafax.game.mancala.MancalaBuilder;
 import org.effrafax.game.mancala.domain.Player;
@@ -19,8 +20,19 @@ import org.effrafax.game.mancala.domain.Player;
 public class Status extends WebPage {
 	
 	private static final String BOWL_ID = "BowlNr";
+	private static final String STONES_ID = "StonesNr";
 	private static final String KALAHA_ID = "Kalaha";
-	
+
+	public Status() {
+		
+		showStatus();
+	}
+
+	/**
+	 * Returns the current mancala game.
+	 * 
+	 * @return the current mancala game.
+	 */
 	public Mancala getMancala() {
 		
 		MancalaBuilder builder = new MancalaBuilder();
@@ -31,11 +43,9 @@ public class Status extends WebPage {
 		return new Mancala(builder);
 	}
 	
-	public Status() {
-		
-		showStatus();
-	}
-	
+	/**
+	 * Shows a representation the current mancala game.
+	 */
 	public void showStatus() {
 		
 		Mancala mancala = getMancala();
@@ -50,22 +60,55 @@ public class Status extends WebPage {
 			List<Integer> stones = stonesPerPlayer.get(player);
 			for (int index = 0; index < 6; index++) {
 				
-				String currentBowlId = player.toString() + BOWL_ID + index;
+				String currentStonesId = player.toString() + STONES_ID + index;
 				
 				Integer numberOfStones = stones.get(index);
-				if (numberOfStones == 0 || ! player.equals(currentPlayer)) {
+				if (numberOfStones > 0 && player.equals(currentPlayer)) {
 					
-					/* This bowl is not playable. */
-					add(new Label(currentBowlId, stones.get(index).toString()));
+					/* This is a playable bowl. */
+					createLink(player, index);
 				} else {
-					
-					/* This bowl is playable. */
-					add(new Label(currentBowlId, "-" + stones.get(index)));
+				
+					add(new Label(currentStonesId, stones.get(index).toString()));
 				}
 			}
 			
 			String kalahaId = player.toString() + KALAHA_ID;
 			add(new Label(kalahaId, stones.get(6).toString()));
 		}
+	}
+	
+	/**
+	 * Creates a link on a a bowl. This should be used if the 
+	 * bowl is playable.
+	 * 
+	 * @param player The {@code Player} who owns the bowl.
+	 * @param index The index of the bowl.
+	 */
+	@SuppressWarnings("serial")
+	public void createLink(final Player player, final int index) {
+		
+		String bowlId = player.toString() + STONES_ID + index;
+		
+		add((new Link(bowlId) {
+
+			/* (non-Javadoc)
+			 * @see org.apache.wicket.markup.html.link.Link#onClick()
+			 */
+			@Override
+			public void onClick() {
+				
+				Mancala mancala = getMancala();
+				
+				mancala.play(index);
+				if (mancala.isFinished()) {
+					
+					setResponsePage(Result.class);
+				} else {
+					
+					/* We are not finished so return to this page. */
+				}
+			}
+		}));
 	}
 }
